6613b5b1f6eb13aaabe81c6f2e8aacb7b090e220
Diogo Leal <diogo@diogoleal.com>
2015-01-18 23:42:34 -0200

    improvements in layout and shortcuts

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,5 +1,5 @@
 # Based on tin.py:
-# https://github.com/qtile/qtile-examples/blob/master/user-configs/tin.py
+# https://github.com/qtile/qtile-examples/blob/master/tin.py
 
 try:
     from libqtile.manager import Key, Group
@@ -44,35 +44,36 @@ keys = [
         [sup, "shift"], "Return",
         lazy.layout.toggle_split()
     ),
-    Key([sup], "Return", lazy.spawn("terminator")),
-    Key([sup], "i", lazy.spawn("thunderbird")),
-    Key([sup], "x", lazy.spawn("xchat")),
-    Key([sup], "c", lazy.spawn("chromium")),
+    Key([sup], "Return", lazy.spawn("urxvtc")),
+    Key([sup], "f", lazy.spawn("firefox")),
+    Key([sup], "z", lazy.spawn("spacefm")),
+    Key([sup, 'control'], "l", lazy.spawn("xtrlock")),
     Key([sup], "p", lazy.spawn("pidgin")),
-    Key([sup], "e", lazy.spawn("emacsclient -nc")),
+    Key([sup], "s", lazy.spawn("subl")),
     Key([sup], 'r', lazy.spawncmd()),
     Key([sup], 'w', lazy.window.kill()),
     Key([sup, 'control'], 'r', lazy.restart()),
     Key([sup, 'control'], 'q', lazy.shutdown()),
     Key(
-            [], "XF86AudioRaiseVolume",
-            lazy.spawn("amixer -c 0 -q set Master 2dB+")
+        [], "XF86AudioRaiseVolume",
+        lazy.spawn("amixer -c 0 -q set Master 2dB+")
     ),
     Key(
-            [], "XF86AudioLowerVolume",
-            lazy.spawn("amixer -c 0 -q set Master 2dB-")
+        [], "XF86AudioLowerVolume",
+        lazy.spawn("amixer -c 0 -q set Master 2dB-")
     ),
 
     # cycle to previous group
     Key([sup], "Left", lazy.group.prevgroup()),
     # cycle to next group
     Key([sup], "Right", lazy.group.nextgroup()),
-
     # windows style alt-tab/alt-shift-tab
     Key([sup], "Tab", lazy.nextlayout()),
     Key([sup, "shift"], "Tab", lazy.previouslayout()),
     # PRINT SCREEN
-    Key([sup], "F10", lazy.spawn("import -window root ~/Pictures/Screenshots/screenshot.png")),
+    Key([sup], "F10", lazy.spawn(
+        "scrot '%Y-%m-%d_$wx$h.png' -e 'mv $f  ~/Pictures'")),
+
     Key([alt], "t", lazy.window.toggle_floating()),
 ]
 
@@ -96,45 +97,54 @@ for i in groups:
 
 # This allows you to drag windows around with the mouse if you want.
 mouse = [
-            Drag([sup], "Button1", lazy.window.set_position_floating(),
-                         start=lazy.window.get_position()),
-            Drag([sup], "Button3", lazy.window.set_size_floating(),
-                         start=lazy.window.get_size()),
-            Click([sup], "Button2", lazy.window.bring_to_front())
+    Drag([sup], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([sup], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([sup], "Button2", lazy.window.bring_to_front())
 ]
 
-layouts = [
+
+layouts = (
+    layout.Tile(ratio=0.5),
     layout.Max(),
-    layout.TreeTab(sections=['Work', 'Messaging', 'Docs', 'Util', 'Other']),
-    # a layout for pidgin
-    layout.Slice('right', 256, name='pidgin', role='buddy_list',
-         fallback=layout.Stack(stacks=2, border_width=1)),
-    layout.Tile(ratio=0.35, borderwidth=1),
-]
+    layout.RatioTile(),
+    layout.Matrix(),
+    layout.MonadTall(),
+    layout.Stack(),
+    layout.Zoomy(),
+)
 
 screens = [
     Screen(
-        bottom = bar.Bar(
-                    [
-                        widget.GroupBox(fontsize=10),
-                        widget.WindowName(),
-                        widget.Sep(),
-                        widget.CPUGraph(samples=50, line_width=1, width=50,
-                                        graph_color='FF2020',
-                                        fill_color='C01010'),
-                        widget.MemoryGraph(samples=50, line_width=1, width=50,
-                                           graph_color='0066FF',
-                                           fill_color='001188'),
-                        widget.NetGraph(samples=50, line_width=1,
-                                        width=50, interface="eth0",
-                                        graph_color='22FF44',
-                                        fill_color='11AA11'),
-                        widget.Prompt(),
-                        widget.Systray(),
-                        widget.Clock('%d-%m-%Y %a %I:%M %p', fontsize=12),
-                    ],
-                    25,
-                ),
+        top=bar.Bar(
+            [
+                widget.GroupBox(fontsize=10),
+                widget.Sep(),
+                widget.CurrentLayout(fontsize=10),
+                widget.Sep(),
+                widget.WindowName(fontsize=12),
+                widget.Sep(),
+                widget.Prompt(fontsize=12),
+                widget.CPUGraph(frequency=1, samples=50, line_width=1, width=50,
+                                graph_color='FF2020',
+                                fill_color='C01010'),
+                widget.MemoryGraph(frequency=1, samples=50, line_width=1, width=50,
+                                   graph_color='0066FF',
+                                   fill_color='001188'),
+                widget.NetGraph(frequency=1, samples=50, line_width=1,
+                                width=50, interface="eth0",
+                                graph_color='22FF44',
+                                fill_color='11AA11'),
+                widget.Sep(),
+                widget.YahooWeather(
+                    location='Rio de Janeiro, RJ', update_interval=10, fontsize=10),
+                widget.Systray(),
+                widget.Sep(),
+                widget.Clock('%d-%m-%Y %a %I:%M %p', fontsize=12),
+            ],
+            25,
+        ),
     ),
 ]
 
@@ -161,33 +171,14 @@ def show_shortcuts():
     return lazy.spawn("xterm -wf -e less {0}".format(shortcuts_path))
 
 
-def window_sorter(win):
-    patterns = (
-        ('Gajim', 'Messaging'),
-        ('XChat', 'Messaging'),
-        ('Icedove', 'Messaging'),
-        ('pidgin', 'Messaging'),
-        ('Vimperator', 'Util'),
-        ('Krusader', 'Util'),
-        ('playout', 'Work'),
-        ('lifia', 'Work'),
-        )
-    for k, v in patterns:
-        if k in win.name:
-            return v
-    return 'Other'
-
 @hook.subscribe.client_new
 def dialogs(window):
     if(window.window.get_wm_type() == 'dialog'
-        or window.window.get_wm_transient_for()):
+            or window.window.get_wm_transient_for()):
         window.floating = True
 
-# More shortcuts
-keys.append(Key([alt], "r", lazy.layout.sort_windows(window_sorter)))
+# More shortcut
 keys.append(Key([sup], "h", show_shortcuts()))
-keys.append(Key([sup], "l", lazy.spawn("xscreensaver-command -lock")))
-keys.append(Key([sup], "s", lazy.spawn("xscreensaver -no-splash")))
 
 main = None
 follow_mouse_focus = True
