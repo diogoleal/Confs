fd08ebe3abe19376e46900330d0fe62e87b79ba4
Diogo Leal <diogo@diogoleal.com>
2015-07-22 13:27:18 -0300

    improvements

--- install.sh
+++ install.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
+# Script for install my confs
 
-# Script for nstall my confs
-
-# Install Oh-my-zsh
-git clone $ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+# Zsh
+mkdir ~/.zsh-antigen
+cp -v .zshrc ~/
+curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > ~/.zsh-antigen/antigen.zsh
 
 # tmux
 git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux/themepack
+cp -v .tmux.conf ~/.tmux.conf
 
-cp .Xmodmap .Xresources .emacs  .gitconfig .muttrc .tmux.conf .vimrc .xinitrc .zshrc ~/
-
+# Vim
+cp -v .vimrc ~/
 mkdir ~/.vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-#git clone https://github.com/morhetz/gruvbox.git
-#mv gruvbox/colors gruvbox/autoload ~/.vim/
-#rm -rf gruvbox
-
-#git clone https://github.com/Valloric/YouCompleteMe.git
 vim +PluginInstall +qall
 
+# Awesome
 git clone http://git.sysphere.org/vicious ~/.config/awesome/
-cp -rf .config/awesome/* ~/.config/awesome
+cp -rfv .config/awesome/* ~/.config/awesome
+
+# Irssi
+cp -rfv .irssi ~/
+
+# Others Confs
+cp -v .Xmodmap .Xresources .gitconfig .xinitrc ~/
 
