3476aa9d12d69a2b301f7c5786e7faf7198b71bc
Diogo Leal <diogo@diogoleal.com>
2015-10-11 06:52:38 -0300

    added install packages

--- install.sh
+++ install.sh
@@ -1,6 +1,41 @@
 #!/bin/sh
 # Script for install my confs
 
+
+# xbps conf
+cp -rfv usr/share/xbps.d/xbps.conf /usr/share/xbps.d/xbps.conf
+
+pkgs='
+    mutt-kz
+    zsh
+    git
+    mpd
+    vim-huge
+    irssi
+    ncmpcpp
+    i3
+    i3lock
+    i3status
+    tmux
+    rxvt-unicode
+    rxvt-unicode-terminfo
+    urxvt-perls
+    git-extras
+    tig
+    wicd
+    xombrero
+    xbacklight
+    feh
+    xbanish
+    rtorrent
+    newsbeuter
+    task
+    htop
+    profanity
+'
+echo $pkgs
+xbps-install --yes $pkgs
+
 # Zsh
 mkdir ~/.zsh-antigen
 cp -v .zshrc ~/
@@ -11,18 +46,18 @@ git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux/themepack
 cp -v .tmux.conf ~/.tmux.conf
 
 # Vim
-cp -v .vimrc ~/
 mkdir ~/.vim
+cp -v .vimrc ~/
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-# Awesome
-git clone http://git.sysphere.org/vicious ~/.config/awesome/
-cp -rfv .config/awesome/* ~/.config/awesome
-
-# Irssi
-cp -rfv .irssi ~/
-
 # Others Confs
-cp -v .Xmodmap .Xresources .gitconfig .xinitrc ~/
+cp -vrf .Xmodmap .Xresources .gitconfig .xinitrc .xombrero .rtorrent.rc .irssi .taskrc\
+.muttrc .mpd.conf .config/htop .newsbeuter ~/
+
+#my bin files
+cp bin/ ~/bin/
+
+#i3
+cp -rfv .i3 .i3status.conf ~/
 
