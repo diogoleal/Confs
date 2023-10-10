b133477e7b9e13e632935b4b37e01828b202d749
Diogo Leal <diogo@diogoleal.com>
2023-10-10 20:29:05 -0300

    fixed neovim install plugins

--- setup.sh
+++ setup.sh
@@ -104,7 +104,7 @@ mkdir -p "$HOME"/.config/nvim && true
 ln -s "$DIR_CONF"/.vimrc "$HOME"/.config/nvim/init.vim
 curl -fLo "$HOME"/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+nvim +PlugInstall +qall
 
 # asdf plugins
 # Terraform
