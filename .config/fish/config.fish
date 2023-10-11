2ddaa137bc08a353a1155b36e8672389c858aea4
Diogo Leal <diogo@diogoleal.com>
2023-10-10 21:03:17 -0300

    added fish_add_path to ~/bin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 direnv hook fish | source
+fish_add_path /home/diogo/bin/
 
 function fleet
     /home/diogo/.local/share/JetBrains/Toolbox/scripts/fleet
@@ -8,9 +9,9 @@ function pycharm
     /home/diogo/.local/share/JetBrains/Toolbox/scripts/pycharm
 end
 
-#function _
-#  sudo $argv
-#end
+function S
+  sudo $argv
+end
 
 function tempo
   curl -4 http://wttr.in/RiodeJaneiro
@@ -24,7 +25,3 @@ function sprunge
   curl -F 'sprunge=<-' http://sprunge.us
 end
 
-# pyenv init
-#if command -v pyenv 1>/dev/null 2>&1
-#  pyenv init - | source
-#end
