664f2af6aa49404665db792b946482d8588798fd
Diogo Leal <diogo@diogoleal.com>
2023-10-12 15:06:34 -0300

    added gsettings

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
 export BIN="$HOME/bin/"
-TERRAFORM_VERSION="latest"
+DIR_CONF="$HOME/Workspace/Confs"
+TERRAFORM_VERSION=latest
 HELM_VERSION="latest"
 ASDF_VERSION=v0.13.1
-DIR_CONF="$HOME/Workspace/Confs"
-HACK_VERSION="v3.003"
-KIND_VERSION="v0.20.0"
-JETBRAINS_TOOLBOX_VERSION="2.0.4.17212"
+HACK_VERSION=v3.003
+KIND_VERSION=v0.20.0
+JETBRAINS_TOOLBOX_VERSION=2.0.4.17212
 
 mkdir ~/bin -p && mkdir -p ~/lib || true
 
-echo "install fzf, direnv, Docker, Discord, Telegram, Font Hack... ;)"
-sudo dnf install curl git fzf direnv gnome-tweaks util-linux-user delta-git moreutils podman fish \
-    openssl-libs zlib-devel clang clang-devel bzip2-devel libffi-devel readline-devel sqlite-devel -y
+echo "install packages via dnf ;)"
+sudo dnf update -y
+sudo dnf install curl git fzf direnv gnome-tweaks util-linux-user delta-git \
+                      moreutils podman fish openssl-libs zlib-devel clang \
+                      clang-devel bzip2-devel libffi-devel readline-devel sqlite-devel -y
 
 sudo systemctl enable --now podman.socket
 
@@ -28,12 +30,6 @@ ln -sf "$DIR_CONF"/.config/fish/functions/alias.fish ~/.config/fish/functions/al
 ln -sf "$DIR_CONF"/.config/fish/config.fish ~/.config/fish/config.fish
 curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 fish -c "fisher install laughedelic/pisces"
-
-# Python
-pip install --upgrade pip
-# python3 -m pip install --user pipx
-pip install pylint
-pip install git+https://github.com/psf/black
 
 # kubectl
 curl -sLO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -51,7 +47,7 @@ fi
 # Flatpak
 flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-for i in org.signal.Signal com.discordapp.Discord org.telegram.desktop org.mozilla.firefox com io.podman_desktop.PodmanDesktop com.brave.Browser com.google.Chrome com.github.tchx84.Flatseal dev.geopjr.Tuba
+for i in org.signal.Signal org.telegram.desktop io.podman_desktop.PodmanDesktop com.brave.Browser com.google.Chrome dev.geopjr.Tuba
 do
     flatpak -y --user install $i
 done
@@ -82,7 +78,6 @@ if [ ! -d "${HOME}"/.asdf ]; then
     mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 fi
 
-
 # neovim
 sudo dnf install -y neovim
 mkdir -p "$HOME"/.config/nvim && true
@@ -92,27 +87,27 @@ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall
 
 # asdf plugins
-# Terraform
+## terraform
 asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
 asdf install terraform $TERRAFORM_VERSION
 asdf global terraform $TERRAFORM_VERSION
-# helm
+## helm
 asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
 asdf install helm $HELM_VERSION
 asdf global helm $HELM_VERSION
-# awscli 2
-asdf plugin add awscli
+## awscli 2
+asdf plugin-add awscli
 asdf install awscli latest:2
 asdf global awscli latest
-# aws-vault
+## aws-vault
 asdf plugin-add aws-vault https://github.com/karancode/asdf-aws-vault.git
 asdf install aws-vault latest
 asdf global aws-vault latest
-# eksctl
+## eksctl
 asdf plugin-add eksctl https://github.com/elementalvoid/asdf-eksctl.git
 asdf install eksctl latest
 asdf global aws-vault latest
-# heptio-authenticator-aws
+## heptio-authenticator-aws
 asdf plugin-add heptio-authenticator-aws https://github.com/neerfri/asdf-heptio-authenticator-aws.git
 asdf install heptio-authenticator-aws latest
 asdf global heptio-authenticator-aws latest
@@ -122,3 +117,13 @@ wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-${JETBRAINS_TOOLBO
 tar -xvf jetbrains-toolbox-${JETBRAINS_TOOLBOX_VERSION}.tar.gz
 jetbrains-toolbox-${JETBRAINS_TOOLBOX_VERSION}/jetbrains-toolbox
 rm -rf jetbrains-toolbox-${JETBRAINS_TOOLBOX_VERSION}.tar.gz
+
+# GNOME
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.shell favorite-apps "[
+                                              'com.brave.Browser.desktop',
+                                              'org.gnome.Nautilus.desktop',
+                                              'org.gnome.Boxes.desktop',
+                                              'jetbrains-pycharm-ce.desktop',
+                                              'com.gexperts.Tilix.desktop',
+                                              'virt-manager.desktop']"
