e6868bfffae866a3b8fe15c81b6d9076b5ef6899
Diogo Leal <diogo@diogoleal.com>
2025-02-23 01:27:03 -0300

    fixed

--- gnome.sh
+++ gnome.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIR_EXTENSIONS="/home/diogo/.local/share/gnome-shell/extensions"
+DIR_EXTENSIONS="~/.local/share/gnome-shell/extensions"
 
 # GNOME extensions
 git clone https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git ${DIR_EXTENSIONS}/clipboard-indicator@tudmotu.com
@@ -15,12 +15,12 @@ gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.shell favorite-apps "[
-                                              'com.vivaldi.Vivaldi.desktop',
+                                              'vivaldi-stable.desktop',
                                               'org.gnome.Nautilus.desktop',
                                               'org.gnome.Ptyxis.desktop',
                                               'sublime_text.desktop',
                                               'net.cozic.joplin_desktop.desktop',
                                               'io.ente.auth.desktop',
-                                              'org.telegram.desktop'.
+                                              'org.telegram.desktop',
                                               'org.onlyoffice.desktopeditors.desktop']"
 gnome-session-quit --logout
